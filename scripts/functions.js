244936b4ccbdd6f5d5a273235c26463bfe8f8fe4
jemapau <je.mapau@gmail.com>
2016-06-15 19:55:00 -0500

    List people data .js

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,9 +1,48 @@
-
-function login () {
-	var username
-	var password
-
-	username = "maria"
-	password = "123"
-	alert (username + '' + password);
+function greetings() {
+  window.alert('Hi there, you want to log in !');
 };
+
+function validate () {
+  var username, password
+
+  username = document.getElementById('username').value
+  password = document.getElementById('password').value
+
+  if ((username === undefined || username === '')
+                  || (password === undefined || password === '')) {
+    document.getElementById('error').style.color = "red"
+    document.getElementById('error').innerHTML = 'Username or Password are invalid !.'
+  } else {
+    document.getElementById('error').style.color = "green"
+    document.getElementById('error').innerHTML = 'Username and Password are valid !.'
+  };
+};
+
+function listarPeople() {
+	var people = [{nombre: 'Carlos', apellido: 'Ruiz' + ' ' + 'Gómez', edad: '25 años'},
+				{nombre: 'Maria', apellido: 'Perez' + ' ' + 'Atehortúa', edad: '28 años'}]
+
+	var table = document.getElementById ('people-table')
+	for(var i = 0; i < people.length; i++) {
+		var row = table.insertRow(0)
+		var cell0 = row.insertCell(0)
+		var cell1 = row.insertCell(1)
+		var cell2 = row.insertCell(2)
+		cell0.innerHTML = people[i].nombre
+		cell1.innerHTML = people[i].apellido
+		cell2.innerHTML = people[i].edad
+	}
+}
+
+function numeroPar(numero) {
+
+	var result = false;
+
+	if(numero % 2 === 0) {
+		result	= true;
+	} else {
+		result = false ;
+	}
+
+	return result
+}
